da945e7776d3841ed5e863fbc9efaf34855c7dc3
phil olson <philip.olson@protonmail.ch>
2016-12-02 18:09:29 -0800

    get a working '/Default' page  - check for null in Global.asax.cs so an empty page doesn't appear  - reinstall missing scriptmanager dependencies
    misc
     - remove some commented code (commented code should not be committed)

--- WMTA/Global.asax.cs
+++ WMTA/Global.asax.cs
@@ -25,22 +25,23 @@ namespace WMTA
             Exception exc = Server.GetLastError();
 
             // Handle HTTP errors
-            if (exc.GetType() == typeof(HttpException))
+            if (exc != null && exc.GetType() == typeof(HttpException))
             {
                 //Redirect HTTP errors to HttpError page
                 Server.Transfer("HttpErrorPage.aspx");
             }
 
+            String msg = (exc == null)
+                ? "Unkown Error"
+                : exc.Message;
+
             // For other kinds of errors give the user some information
-            // but stay on the default page
+            //   but stay on the default page
             Response.Write("<h2>Global Page Error</h2>\n");
             Response.Write(
-                "<p>" + exc.Message + "</p>\n");
+                "<p>" + msg + "</p>\n");
             Response.Write("Return to the <a href='~/WelcomeScreen.aspx'>" +
                 "Home Page</a>\n");
-
-            // Log the exception 
-            //Utility.LogError("Global.asax", "", "", exc.Message, -1);
 
             // Clear the error from the server
             Server.ClearError();
